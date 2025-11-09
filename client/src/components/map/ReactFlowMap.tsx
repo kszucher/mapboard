5718d9df9439e287d854a00db28e16031cef183b
krisz <krisztian@szucher.com>
2025-11-09 14:59:31 +0100

    fit and center

--- client/src/components/map/ReactFlowMap.tsx
+++ client/src/components/map/ReactFlowMap.tsx
@@ -5,11 +5,13 @@ import {
   NodeTypes,
   Position,
   ReactFlow,
+  ReactFlowProvider,
   useEdgesState,
   useNodesState,
+  useReactFlow,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
-import { useCallback, useEffect } from 'react';
+import { MouseEvent, useCallback, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useGetToolInfoQuery } from '../../data/api.ts';
 import { RootState } from '../../data/store.ts';
@@ -18,18 +20,33 @@ import { AppFlowEdge, AppFlowNode } from './types.ts';
 
 const nodeTypes: NodeTypes = { custom: CustomNode };
 
-export const ReactFlowMap = () => {
+const FlowContent = () => {
   const m = useSelector((state: RootState) => state.slice.commitList[state.slice.commitIndex]);
   const tools = useGetToolInfoQuery().data;
-
   const [nodes, setNodes, onNodesChange] = useNodesState<AppFlowNode>([]);
   const [edges, setEdges, onEdgesChange] = useEdgesState<AppFlowEdge>([]);
+  const { fitView } = useReactFlow();
 
   const onConnect = useCallback((params: Connection) => setEdges(els => addEdge(params, els)), []);
 
+  const centerAndFit = useCallback(() => {
+    fitView({
+      padding: 0.1,
+      maxZoom: 1,
+      duration: 300,
+    }).then();
+  }, [fitView]);
+
+  const handleDoubleClick = useCallback(
+    (event: MouseEvent) => {
+      event.preventDefault();
+      centerAndFit();
+    },
+    [centerAndFit]
+  );
+
   useEffect(() => {
     if (!m || !tools) return;
-
     const mappedNodes: AppFlowNode[] = m.n.map(n => ({
       id: n.id.toString(),
       type: 'custom',
@@ -38,7 +55,6 @@ export const ReactFlowMap = () => {
       sourcePosition: Position.Right,
       targetPosition: Position.Left,
     }));
-
     const mappedEdges: AppFlowEdge[] = m.e.map(e => ({
       id: e.id.toString(),
       source: e.fromNodeId.toString(),
@@ -49,29 +65,43 @@ export const ReactFlowMap = () => {
         strokeWidth: e.lineWidth || 1,
       },
     }));
-
     setNodes(mappedNodes);
     setEdges(mappedEdges);
-  }, [m, tools, setNodes, setEdges]);
+
+    fitView({
+      padding: 0.1,
+      maxZoom: 1,
+      duration: 0,
+    }).then();
+  }, [m, tools, setNodes, setEdges, fitView]);
 
   if (!m) return null;
 
   return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      nodeTypes={nodeTypes}
+      onConnect={onConnect}
+      onDoubleClick={handleDoubleClick}
+      colorMode="dark"
+      zoomOnScroll={false}
+      zoomOnDoubleClick={false}
+      panOnScroll
+    >
+      <Controls />
+    </ReactFlow>
+  );
+};
+
+export const ReactFlowMap = () => {
+  return (
     <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        nodeTypes={nodeTypes}
-        onConnect={onConnect}
-        fitView
-        colorMode="dark"
-        zoomOnScroll={false}
-        panOnScroll
-      >
-        <Controls />
-      </ReactFlow>
+      <ReactFlowProvider>
+        <FlowContent />
+      </ReactFlowProvider>
     </div>
   );
 };
